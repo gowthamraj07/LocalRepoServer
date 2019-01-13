1bb2816abb7a455e2f37eb5b465bd7b3d1ec97ee
Gowtham Shanmugaraj Ganesan <gowthamraj07@gmail.com>
2019-01-13 13:24:43 +0100

    Test case refactored

--- src/test/java/com/localrepo/server/URLControllerTest.java
+++ src/test/java/com/localrepo/server/URLControllerTest.java
@@ -2,6 +2,7 @@ package com.localrepo.server;
 
 import com.localrepo.server.domain.DependencyDomain;
 import com.localrepo.server.repository.DependencyRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
@@ -10,12 +11,19 @@ import java.io.PrintWriter;
 
 public class URLControllerTest {
 
+    private URLController controller;
+    private PrintWriter writer;
+    private DependencyRepository repository;
+
+    @Before
+    public void setUp() {
+        repository = Mockito.mock(DependencyRepository.class);
+        writer = Mockito.mock(PrintWriter.class);
+        controller = new URLController(writer, repository);
+    }
+
     @Test
     public void shouldPrintTheRequestURL() {
-        PrintWriter writer = Mockito.mock(PrintWriter.class);
-        DependencyRepository repository = Mockito.mock(DependencyRepository.class);
-        URLController controller = new URLController(writer, repository);
-
         String path = "any path";
         controller.getDependency(path);
 
@@ -24,13 +32,10 @@ public class URLControllerTest {
 
     @Test
     public void shouldMakeRepositoryCallToFetchId() {
-        DependencyRepository repository = Mockito.mock(DependencyRepository.class);
-        PrintWriter writer = Mockito.mock(PrintWriter.class);
-        URLController controller = new URLController(writer, repository);
-
         String path = "any path";
         controller.getDependency(path);
 
         Mockito.verify(repository).getId(ArgumentMatchers.any(DependencyDomain.class));
     }
+
 }
