91793a175c14041490fa041d011198815c4f2b99
Gowtham Shanmugaraj Ganesan <gowthamraj07@gmail.com>
2019-01-13 23:25:19 +0100

    Annotations added to find Entitiy, JPA Repositories and Controllers

--- src/main/java/com/localrepo/server/ServerApplication.java
+++ src/main/java/com/localrepo/server/ServerApplication.java
@@ -2,10 +2,14 @@ package com.localrepo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.localrepo.server.repository")
+@ComponentScan({"com.localrepo.server", "com.localrepo.server.repository"})
+@EntityScan({"com.localrepo.server.domain"})
+@EnableJpaRepositories({"com.localrepo.server.repository"})
 public class ServerApplication {
 
 	public static void main(String[] args) {
