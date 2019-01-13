a23872391e445ddcc017682a144318916d8ce693
Gowtham Shanmugaraj Ganesan <gowthamraj07@gmail.com>
2019-01-13 20:56:07 +0100

    File repository logic added

--- src/main/java/com/localrepo/server/repository/FileRepository.java
+++ src/main/java/com/localrepo/server/repository/FileRepository.java
@@ -1,12 +1,17 @@
 package com.localrepo.server.repository;
 
+import java.io.File;
+
 public class FileRepository {
     public void createDirectory(String directoryId) {
-
+        File file = new File(getRepoDirectoryPath() + directoryId);
+        if (!file.exists()) {
+            file.mkdir();
+        }
     }
 
     public boolean isDirectoryExists(String path) {
-        return false;
+        return new File(path).exists();
     }
 
     public String getRepoDirectoryPath() {
