be12cca52426ae6b8d38eb23ce47bfe3ad5584cf
Gowtham Shanmugaraj Ganesan <gowthamraj07@gmail.com>
2019-01-14 23:04:49 +0100

    function added to return domain list by path

--- src/main/java/com/localrepo/server/repository/DependencyCrudRepository.java
+++ src/main/java/com/localrepo/server/repository/DependencyCrudRepository.java
@@ -4,8 +4,12 @@ import com.localrepo.server.domain.DependencyDomain;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DependencyCrudRepository extends CrudRepository<DependencyDomain, Long> {
     @Override
     Iterable<DependencyDomain> findAll();
+
+    List<DependencyDomain> findByPath(String path);
 }
